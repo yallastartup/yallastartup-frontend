c7a741f8e3893cd5ad67cb60d4d03e8967ab9d0d
Dalia Mohamed <daliametwaly88@gmail.com>
2020-04-04 21:00:42 +0200

    fix app

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,51 +1,51 @@
-// const express=require('express');
-// const cookieSession = require('cookie-session');
-// const bcrypt = require('bcryptjs');
-// var myParser = require("body-parser");
-// const { body, validationResult } = require('express-validator');
-// const another = require('../index');
-// const router=new express.Router();
-// const upload = require('../uploadMiddleware');
-// const Resize = require('../Resize');
-// var path=require('path');
-// var reg_info=require('../../public/dist/js/api/request');
-// router.use(myParser.urlencoded({extended : true}));
-// commonFunc=require('./commonFunc')
-// //=============================================
-// //==================user.js===========================
-// //get all users
-// router.get('/admins',commonFunc.ifNotLoggedin,async(req,res)=>{
+const express=require('express');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcryptjs');
+var myParser = require("body-parser");
+const { body, validationResult } = require('express-validator');
+const another = require('../index');
+const router=new express.Router();
+const upload = require('../uploadMiddleware');
+const Resize = require('../Resize');
+var path=require('path');
+var reg_info=require('../../public/dist/js/api/request');
+router.use(myParser.urlencoded({extended : true}));
+commonFunc=require('./commonFunc')
+//=============================================
+//==================user.js===========================
+//get all users
+router.get('/admins',commonFunc.ifNotLoggedin,async(req,res)=>{
 
-//     try
-//     {
-//         res.render('admins/view.hbs', {});
-//     }
-//     catch(e)
-//     {
-//         res.status(500).send()
+    try
+    {
+        res.render('admins/view.hbs', {});
+    }
+    catch(e)
+    {
+        res.status(500).send()
 
-//     }
+    }
 
-// });
-// //=============================================
-// //add user
-// router.get('/add_admin',commonFunc.ifNotLoggedin,async (req, res) => {
-//     //API CALL
-//     res.render('admins/add.hbs', {
+});
+//=============================================
+//add user
+router.get('/add_admin',commonFunc.ifNotLoggedin,async (req, res) => {
+    //API CALL
+    res.render('admins/add.hbs', {
 
-//     });
-// });
-// //=============================================
-// //edit user
-// router.get('/admin/edit',commonFunc.ifNotLoggedin, (req, res) => {
-//     var id = req.query.id;
-//     res.render( 'admins/edit.hbs',{id:id});
-// });
-// //=============================================
-// //edit admin profile user
-// router.get('/profile',commonFunc.ifNotLoggedin, (req, res) => {
-//     var id = req.query.id;
-//     res.render( 'admins/superAdminProfile.hbs',{id:id});
-// });
-// //===============================
-// module.exports=router;
+    });
+});
+//=============================================
+//edit user
+router.get('/admin/edit',commonFunc.ifNotLoggedin, (req, res) => {
+    var id = req.query.id;
+    res.render( 'admins/edit.hbs',{id:id});
+});
+//=============================================
+//edit admin profile user
+router.get('/profile',commonFunc.ifNotLoggedin, (req, res) => {
+    var id = req.query.id;
+    res.render( 'admins/superAdminProfile.hbs',{id:id});
+});
+//===============================
+module.exports=router;
