4863099213f4e80d4b8807eeeb9b9854038179bd
Dalia Mohamed <daliametwaly88@gmail.com>
2020-04-10 23:01:02 +0200

    landing page

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -5,10 +5,8 @@ var myParser = require("body-parser");
 const { body, validationResult } = require('express-validator');
 const another = require('../index');
 const router=new express.Router();
-const upload = require('../uploadMiddleware');
-const Resize = require('../Resize');
 var path=require('path');
-var reg_info=require('../../public/dist/js/api/request');
+
 router.use(myParser.urlencoded({extended : true}));
 commonFunc=require('./commonFunc')
 //=============================================
